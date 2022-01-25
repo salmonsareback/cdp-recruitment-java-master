7e6cf2f1120e4e30dd0e78e8331a121d4f0a71f2
L Duc <salmonsareback007@gmail.com>
2022-01-25 12:19:20 +0100

    Initial commit

---
+++ src/main/java/adeo/leroymerlin/cdp/EventRepository.java
@@ -0,0 +1,15 @@
+package adeo.leroymerlin.cdp;
+
+import org.springframework.data.repository.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+public interface EventRepository extends Repository<Event, Long> {
+
+    void deleteById(Long eventId);
+
+    List<Event> findAllBy();
+}

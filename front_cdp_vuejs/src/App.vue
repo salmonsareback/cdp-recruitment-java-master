d9566e6c0b04e7a99f254d0ba0e0c447e61d0fa7
LDuc <salmonsareback007@gmail.com>
2022-03-22 17:13:02 +0100

    Add Module front_cdp_vuejs

---
+++ front_cdp_vuejs/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <div class="page-header">
+      <h1><i class="fa fa-event" aria-hidden="true"></i> My Event</h1>
+    </div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <hello-world msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import helloWorld from "./components/HelloWorld.vue";
+import viewEvents from "./views/ViewEvents.vue";
+
+@Component({components:{helloWorld,viewEvents}})
+export default class App extends Vue {  
+
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Lobster|Open+Sans+Condensed:300|Roboto:500');
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.page-header h1 {
+    font-family: 'Lobster', cursive;
+    color: #4c4c4c;
+}
+</style>
---
+++ front_cdp_vuejs/src/views/ViewEvents.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row>
+      
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+export default class ViewEvents extends Vue {  
+
+}
+</script>
+
+<style>
+
+</style>
